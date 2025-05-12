61d8685232bd1b6e21626311119740775a143473
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2025-05-12 18:46:15 +0300

    add new migration
    Added new columns to exams entity

---
+++ ExaminationSystem.Data/Migrations/20250512153402_m2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExaminationSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class m2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Exams",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "Exams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "Exams",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "PassingMarks",
+                table: "Exams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "Exams",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalMarks",
+                table: "Exams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "PassingMarks",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "TotalMarks",
+                table: "Exams");
+        }
+    }
+}
